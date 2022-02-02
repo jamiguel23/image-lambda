72f4c3335602f02a59a46118b126ae0202638986
jamiguel23 <josematthewmiguel@gmail.com>
2022-02-02 15:41:29 -0800

    now working in AWS

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
 let AWS = require('aws-sdk');
-let s3 = new AWS.S3();
 
+let S3 = new AWS.S3();
 
 exports.handler = async (event) => {
+    console.log(event.Records[0].s3.object);
+    const fileName = event.Records[0].s3.object.key;
+    const fileSize = event.Records[0].s3.object.size;
+    const bucketName = event.Records[0].s3.bucket.name;
     
-    // console.log(event);
-    // console.log('this should be bucketname', event.Records[0].s3.bucket.name)
-    let bucketName = event.Records[0].s3.bucket.name;
-    let fileName = event.Records[0].s3.object.key;
-    let size = event.Records[0].s3.object.size;
-    console.log(fileName, bucketName, size);
+    console.log("BUCKET IS " + bucketName);
     
-    const params = {
-        Bucket: bucketName,
-        Key: 'images.json',
+    let metaData = {
+        name: fileName,
+        size: fileSize,
+        type: "image"
+    };
+    
+    
+    try {
         
+        let manifestBody = await S3.getObject({ 
+            Bucket: bucketName,
+            Key: 'images.json' 
+            
+        }).promise();
+    
+        console.log('this is the manifestBody', manifestBody);
+    
+        let manifestJSON = JSON.parse(manifestBody.Body.toString());
+    
+        manifestJSON.push(metaData);
+    
+        let newManifest = S3.putObject({ 
+            Bucket: bucketName, 
+            Key: 'images.json', 
+            Body: JSON.stringify(manifestJSON) 
+        
+        }).promise();
+        
+        console.log('this is the manifstJSON', manifestJSON);
+        
+    
+        const response = {
+        statusCode: 200,
+        body: JSON.stringify({message: 'itsa me Matt'}),
+    };
+    return response;
+    } catch (e){
+        console.log(e);
+        
+        let manifest = [metaData];
+        
+        let newManifest = await S3.putObject({ 
+            Bucket: bucketName, 
+            Key: 'images.json', 
+            Body: JSON.stringify(manifest) 
+            
+        }).promise();
+        return{
+            message: 'New manifest generated.',
+        }
     }
-  
-  
-  try{
-      
-    const manifest = await s3.getObject(params).promise();
-    let manifestData = JSON.parse(manifest.Body.toString());
 
-    // add the name/ size/ type to our json file
-    manifestData.push({
-      name: fileName,
-      size: size,
-      type: 'image',
-    })
-
-
-    //not getting this console log
-    console.log('current manifest:', manifestData);
-    // let manifestBody = JSON.stringify(manifestData);
-
-    // // write the file back to the bucket
-    // const newManifest = await s3.putObject({ ...params, Body: manifestBody, ContentType: 'application/json' }).promise();
-    // console.log('new manifest:', newManifest);
-      
-  } catch(e){
-      console.log(e);
-      
-    //   const newMani = {
-    //       Bucket: bucketName,
-    //   Key: 'images.json',
-    //   Body: JSON.stringify([{ name: fileName, size: size, type: 'image' }]),
-    //   ContentType: 'application/json',
-    //   }
-    //   const manifest = await s3.putObject(newMani).promise();
-    // console.log('JSON file created from bucket:', manifest);
-  }
-    const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello? Itsa me, Matt'),
-  };
-  return response;
 };
